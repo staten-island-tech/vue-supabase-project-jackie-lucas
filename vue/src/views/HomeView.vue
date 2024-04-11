6b5e4894b0e55bcf0ae588702e5fbf4df332977b
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-04-11 09:24:19 -0400

    wokr
    <template>
      <div>
    <!--     <form class="form" id="form">
                <label for="email">Email:</label>
                <input type="text" class="email" id="email"/>
                <br>
                <label for="password">Password:</label>
                <input type="text" class="password" id="password"/>
                <br>
                <div class="LogIn">
                    <button id="SignIn">Log In</button>
                </div>
            </form> -->
      </div>
    </template>
    
    <script setup>
    
    import { createClient } from '@supabase/supabase-js'
    const supabase = createClient('https://nrcrotoefzmagfjcjkcu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yY3JvdG9lZnptYWdmamNqa2N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNDk5NjAsImV4cCI6MjAyNzcyNTk2MH0.FUW8l47J6vtGxGPTKn5Yw8dEWYZ_WXB0CYGzMfy22yA')
    // imports Supabase and links api to file
    
    /* const { data, error } = await supabase.auth.signUp(
      {
        email: 'test@email.com',
        password: 'test-password',
        options: {
          data: {
            first_name: 'John',
            age: 27,
          }
        }
      }
    );
    
    async function signInWithEmail() {
      const { data, error } = await supabase.auth.signInWithPassword({
        email: 'example@email.com',
        password: 'example-password',
      })
    };
    
    async function signOut() {
      const { error } = await supabase.auth.signOut()
    }; */
    
    async function signUpNewUser() {
      const { data, error } = await supabase.auth.signUp({
        email: 'example@email.com',
        password: 'example-password',
        options: {
          // emailRedirectTo: 'https://example.com/welcome',
        },
      })
    }
    signUpNewUser()
    
    async function signInWithEmail() {
      const { data, error } = await supabase.auth.signInWithPassword({
        email: 'example@email.com',
        password: 'example-password',
      })
    }
    signInWithEmail()
    
    async function signOut() {
      const { error } = await supabase.auth.signOut()
    }
    signOut()
    </script>
    
    <style scoped>
    
    </style>

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,21 +1,61 @@
-  <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from '@/lib/superbaseClient';
+<template>
+  <div>
+<!--     <form class="form" id="form">
+            <label for="email">Email:</label>
+            <input type="text" class="email" id="email"/>
+            <br>
+            <label for="password">Password:</label>
+            <input type="text" class="password" id="password"/>
+            <br>
+            <div class="LogIn">
+                <button id="SignIn">Log In</button>
+            </div>
+        </form> -->
+  </div>
+</template>
 
-  const countries = ref([])
+<script setup>
 
-  async function getCountries() {
-    const { data } = await supabase.from('food').select()
-    countries.value = data
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://nrcrotoefzmagfjcjkcu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yY3JvdG9lZnptYWdmamNqa2N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNDk5NjAsImV4cCI6MjAyNzcyNTk2MH0.FUW8l47J6vtGxGPTKn5Yw8dEWYZ_WXB0CYGzMfy22yA')
+// imports Supabase and links api to file
+
+/* const { data, error } = await supabase.auth.signUp(
+  {
+    email: 'test@email.com',
+    password: 'test-password',
+    options: {
+      data: {
+        first_name: 'John',
+        age: 27,
+      }
+    }
   }
+);
 
-  onMounted(() => {
-    getCountries()
+async function signInWithEmail() {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: 'example@email.com',
+    password: 'example-password',
   })
-  </script>
+};
 
-  <template>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.item_name }}</li>
-    </ul>
-  </template>
+async function signOut() {
+  const { error } = await supabase.auth.signOut()
+}; */
+
+async function signUpNewUser() {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'test@email.com',
+    password: 'test-password',
+    options: {
+      // emailRedirectTo: 'https://example.com/welcome',
+    },
+  })
+}
+signUpNewUser()
+</script>
+
+<style scoped>
+
+</style>
