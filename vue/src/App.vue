bed0e7545678a484b41844d552265b3802d00096
jackiezzhen <jackiezhen31@gmail.com>
2024-04-11 09:24:12 -0400

    s

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,18 +3,6 @@ import { onMounted, ref } from 'vue'
 import Account from './components/Account.vue'
 import Auth from './components/Auth.vue'
 import { supabase } from './supabase'
-
-const session = ref()
-
-onMounted(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
-
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
 </script>
 
 <template>
--- vue/src/supabase.js
+++ vue/src/supabase.js
@@ -1,6 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from "./lib/superbaseClient"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export {supabase}
