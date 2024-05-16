a3c7f9a8bfd93dda338a71ff302cbeb22cdf840e
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-05-16 09:11:21 -0400

    Update AboutView.vue

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -6,17 +6,18 @@ const Upassword = ref("");
 import { supabase } from '@/supabase.js'
 
 async function signInWithEmail() {
+  const logged = false
   const useremail= Uemail.value;
   const userpassword= Upassword.value;
   console.log(useremail, userpassword);
   const { data, error } = await supabase.auth.signInWithPassword({
     email: useremail,
     password: userpassword,
-    //    email: Uemail.toString(),
-    //    password: Upassword.toString(),
-    // DO PINIA LINK AND SHIT FOR WHO IS CURRENTLY LOGED IN SING LOADUSER/CLEARUSER
   })
-};
+  if (error) {
+      alert('Incorrect Login Credentials')
+  };
+}
 </script>
 
 
@@ -24,5 +25,5 @@ async function signInWithEmail() {
   <input v-model="Uemail" type="input" />
   <input v-model="Upassword" type="input" />
   <br>
-  <button v-on:click="signInWithEmail()">HIWORKPLS</button>
+  <button v-on:click="signInWithEmail()">Sign In</button>
 </template>
