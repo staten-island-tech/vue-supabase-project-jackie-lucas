8a6762dce1aef7d971f1554634c734b13672eb1b
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-06-06 17:56:19 -0400

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { onMounted, ref } from 'vue'
-/* import Account from './components/Account.vue'
+import Account from './components/Account.vue'
 import Auth from './components/Auth.vue'
 import { supabase } from './supabase'
 const session = ref()
@@ -14,7 +14,7 @@ onMounted(() => {
   supabase.auth.onAuthStateChange((_, _session) => {
     session.value = _session
   })
-}) */
+})
 </script>
 
 <template>
@@ -99,3 +99,113 @@ nav a:first-of-type {
   }
 }
 </style>
+
+<!-- <script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { onMounted, ref } from 'vue'
+/* import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
+import { supabase } from './supabase'
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+}) */
+</script>
+
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+  </div>
+  <header>
+    <div class="wrapper">
+
+<nav>
+  <RouterLink to="/" class="home"><img id="home" width="200rem" src="../public/Icon_Home.png" alt="Home"></RouterLink>
+</nav>
+</div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+.home {
+  position: absolute;
+  left: 0;
+  top: 0.5rem;
+  z-index: 1;
+  padding: 0;
+  margin: 0;
+  background-color: transparent
+} 
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
+ -->
